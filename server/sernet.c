671dbba260333fec276e8210e6826052408848a6
Zeko <zeko@freecivweb.org>
2018-11-30 22:52:04 -0800

    Add server setting to mask timeout each turn by a random offset

--- server/sernet.c
+++ server/sernet.c
@@ -713,7 +713,7 @@ enum server_events server_sniff_all_input(void)
 	  && S_S_RUNNING == server_state()
 	  && game.server.phase_timer
 	  && (timer_read_seconds(game.server.phase_timer)
-	      > game.tinfo.seconds_to_phasedone)) {
+              > game.tinfo.seconds_to_phasedone + game.server.phase_mask)) {
 	con_prompt_off();
 	return S_E_END_OF_TURN_TIMEOUT;
       }
@@ -902,7 +902,7 @@ enum server_events server_sniff_all_input(void)
       && S_S_RUNNING == server_state()
       && game.server.phase_timer
       && (timer_read_seconds(game.server.phase_timer)
-          > game.tinfo.seconds_to_phasedone)) {
+          > game.tinfo.seconds_to_phasedone + game.server.phase_mask)) {
     return S_E_END_OF_TURN_TIMEOUT;
   }
   if ((game.server.autosaves & (1 << AS_TIMER))
